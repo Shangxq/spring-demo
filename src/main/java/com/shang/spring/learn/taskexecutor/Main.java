965eb71dfbfc8c5ed4000231f33e865d63734669
尚晓琼 <1194254278@qq.com>
2017-12-26 14:30:54 +0800

    异步练习

---
+++ src/main/java/com/shang/spring/learn/taskexecutor/Main.java
@@ -0,0 +1,14 @@
+package com.shang.spring.learn.taskexecutor;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+        AsyncTaskService service=context.getBean(AsyncTaskService.class);
+        for(int i=0;i<10;i++){
+            service.executeAsyncTask(i);
+            service.executeAsyncTaskPlus(i);
+        }
+    }
+}
