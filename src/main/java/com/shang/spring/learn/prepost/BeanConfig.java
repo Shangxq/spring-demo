d7cc302f5ed81c710db7934106771b7c6e200e5a
尚晓琼 <1194254278@qq.com>
2018-02-12 21:54:02 +0800

    I18N (properties 使用ISO 8859-1就不会乱码了)

--- src/main/java/com/shang/spring/learn/prepost/BeanConfig.java
+++ src/main/java/com/shang/spring/learn/prepost/BeanConfig.java
@@ -3,6 +3,7 @@ package com.shang.spring.learn.prepost;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan("com.shang.spring.learn.prepost")
@@ -10,5 +11,11 @@ public class BeanConfig {
     @Bean(initMethod = "init",destroyMethod = "destoryA")
     BeanWayService beanWayService(){
         return new BeanWayService();
+    }
+    @Bean("messageSource")
+    ResourceBundleMessageSource getI18n(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 }
--- src/main/java/com/shang/spring/learn/prepost/Main.java
+++ src/main/java/com/shang/spring/learn/prepost/Main.java
@@ -2,9 +2,14 @@ package com.shang.spring.learn.prepost;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(BeanConfig.class);
+        String filename=context.getMessage("menu.file",new Object[]{"F"},Locale.CHINA);
+        String editName=context.getMessage("menu.edit",null,Locale.US);
+        System.out.println(filename+ editName);
         context.getBean(BeanWayService.class);
         context.getBean(JSR250WayService.class).say();
         context.close();
